30756f622d6af67ec86caabd894f8131b2dcb97b
Eliana <valdezeliana38@gmail.com>
2026-05-19 16:39:05 -0300

    add configuration for database connection using dotenv

---
+++ app/config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = (
+    f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+)
